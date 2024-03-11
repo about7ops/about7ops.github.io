4d0c95a010b484ed116429d786b50ea08b8302aa
about7ops <about7snakes@gmail.com>
2024-03-11 16:07:47 -0400

    create simple navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import MovingHeader from './components/MovingHeader.js';
 // import AnimationChain from './components/MovingHeaderChain.js';
+import Header from './components/Header.js';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MovingHeader />
-      </header>
-    </div>
+  <div className="App">
+  <Header />
+  <MovingHeader />
+  <img src={logo} className="App-logo" alt="logo" />
+  </div>
   );
 }
 
