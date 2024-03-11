a65704035c6ec22b3e5da3ee89ef37a3ff6dd861
about7ops <about7snakes@gmail.com>
2024-03-11 15:25:39 -0400

    test chain animations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import MovingHeader from './components/MovingHeader.js';
+// import AnimationChain from './components/MovingHeaderChain.js';
 
 // move this to components later
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
+// function MyButton() {
+//   return (
+//     <button>
+//       I'm a button
+//     </button>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <MovingHeader />
-        <MyButton />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- my-app/src/components/MovingHeader.js
+++ my-app/src/components/MovingHeader.js
@@ -5,16 +5,16 @@ const MovingHeader = () => {
   return (
   <MovingText
   type="fadeInFromBottom"
-  duration="1200ms"
+  duration="1500ms"
   delay="0s"
   direction="normal"
   timing="ease"
   iteration="1"
   fillMode="none">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore m
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillu
 </MovingText>
 )}
-
-
 
 export default MovingHeader
