779e5ba16807bbeac5bfc7929842711335fcdb58
about7ops <about7snakes@gmail.com>
2024-03-11 18:23:15 -0400

    add text transformations from react-spring

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
+// basic imports
 import profilePic from './images/test_prof_image.jpg';
 import './App.css';
+
+// components
 import FadingText from './components/FadingText.js';
-// import AnimationChain from './components/MovingHeaderChain.js';
 import Header from './components/Header.js';
 
 function App() {
   return (
   <div className="App">
   <Header />
+  <FadingText />
   <img src={profilePic} alt="profile picture" />
   </div>
   );
--- my-app/src/components/FadingText.js
+++ my-app/src/components/FadingText.js
@@ -1,1 +1,40 @@
 import React from 'react'
+import { animated, useSpring } from "@react-spring/web";
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+
+// text should be one col, image should be other
+
+const FadingText = () => {
+  const props = useSpring({
+    
+    opacity: 1,
+
+    // moves object across page
+    transform: "translate(0px, 0px)",
+    from: { opacity: 0, transform: "translate(0px, 200px)" }
+  });
+
+  return (
+    <Container>
+    <Row>
+    <Col>
+    <animated.div style={props}>
+      <div
+        style={{
+          width: "100%",
+          height: "60px",
+          background: "green",
+          marginTop: "20px"
+        }}
+      />
+    </animated.div>
+    </Col>
+    <Col></Col>
+    </Row>
+    </Container>
+  );
+};
+
+export default FadingText
