e95a9141303b420a3796da46bdf4cf70868ba5f4
about7ops <about7snakes@gmail.com>
2024-03-11 16:33:27 -0400

    add test profile image for size reference

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,15 @@
-import logo from './logo.svg';
+import profilePic from './images/test_prof_image.jpg';
 import './App.css';
 import MovingHeader from './components/MovingHeader.js';
 // import AnimationChain from './components/MovingHeaderChain.js';
 import Header from './components/Header.js';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
   <div className="App">
   <Header />
   <MovingHeader />
-  <img src={logo} className="App-logo" alt="logo" />
+  <img src={profilePic} alt="profile picture" />
   </div>
   );
 }
