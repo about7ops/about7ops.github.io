ef8ecb951dfdbc10a6803162eb11b83e8862d285
about7ops <about7snakes@gmail.com>
2024-03-12 12:49:32 -0400

    commit changes before editing bootstrap cols/rows

--- my-app/src/components/FadingText.js
+++ my-app/src/components/FadingText.js
@@ -11,13 +11,13 @@ const FadingText = () => {
     <div>
       <Container expand="lg">
         <Row>
-          <Col>
+          <Col xs={12} sm={10} md={8} lg={6}>
             <h1>Lorem Ipsum Lorem Ipsum Lorem...</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
             <h3>Button1 and Button 2</h3>
           </Col>
-          <Col>
+          <Col xs={12} sm={12} md={12} lg={6}>
           <img src={img} alt="profile picture" />
           </Col>
         </Row>
